5c819a5acb977f2c18df0ad639e9458976d50f9b
poojaptdr <pooja.patidar2121@gmail.com>
2022-06-22 20:12:47 +0530

    tacking input and console

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-// input
-var username =  prompt('give me username')
-// processing
-var welcomeMessage = "this script works " + username;
-// output
-alert(welcomeMessage); 
+// // input
+// var username =  prompt('give me username')
+// // processing
+// var welcomeMessage = "this script works " + username;
+// // output
+// alert(welcomeMessage) 
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput)
+
+function clickHandler(){
+    console.log("clicked");
+    console.log("input", txtInput.value)
+    };
+
+btnTranslate.addEventListener("click", clickHandler);
+
